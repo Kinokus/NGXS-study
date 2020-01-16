99953d6f866f7be4a42bd7f85c3ad0b8a5b51289
kinokus <aleks.kinokus@gmail.com>
2020-01-16 16:51:02 +0200

    auth

--- se-ngxs-study/src/app/weather.service.ts
+++ se-ngxs-study/src/app/weather.service.ts
@@ -2,6 +2,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {GetOneDayWeather, OneDayWeather} from '../actions/weather.actions';
+import {concat} from 'rxjs';
 
 
 @Injectable({
@@ -12,23 +13,43 @@ export class WeatherService {
   private city: any;
   private countryCode: any;
   private consumer_key: any;
+  private consumer_secret: string;
+  private concat = '&';
+  private method = 'GET';
+  private tmpUrl = `https://weather-ydn-yql.media.yahoo.com/forecastrss?location=${this.city},${this.countryCode}&format=json`;
+  private composite_key = encodeURIComponent(this.consumer_secret) + this.concat;
 
 
-  constructor(private http: HttpClient) {
+  private oauth = {
+    'oauth_consumer_key': this.consumer_key,
+    'oauth_nonce': Math.random().toString(36).substring(2),
+    'oauth_signature_method': 'HMAC-SHA1',
+    'oauth_timestamp': new Date().getTime() / 1000,
+    'oauth_version': '1.0'
+  };
 
-  }
 
 
+  merged_arr = Object.keys(this.oauth).sort().map(k =>{
+    return [k + '=' + encodeURIComponent(this.oauth[k])];
+  });
 
-  tmpUrl = `https://weather-ydn-yql.media.yahoo.com/forecastrss?location=${this.city},${this.countryCode}&format=json`;
+
+  private signature_base_str = this.method + concat + encodeURIComponent(this.tmpUrl)  + concat + encodeURIComponent(this.merged_arr.join(concat));
+
+
+  private hash = CryptoJS.HmacSHA1(this.signature_base_str, this.composite_key);
+  private signature = this.hash.toString(CryptoJS.enc.Base64);
+
 
   tmpAuthHeader = 'OAuth ' +
-    `oauth_consumer_key="${this.consumer_key}",` +
+    `oauth_consumer_key="${this.oauth.oauth_consumer_key}",` +
     `oauth_nonce="${Math.random().toString(36).substring(2)}",` +
     `oauth_signature_method="HMAC-SHA1",` +
     `oauth_timestamp="${new Date().getTime() / 1000}",` +
     `oauth_version="1.0",` +
     `oauth_signature="JmnomUHOhGlJ2wD8xxh5uJL4iyo="`;
+
   tmpHeader = `
   -H 'Connection: keep-alive' 
   -H 'Accept: */*' 
@@ -40,6 +61,9 @@ export class WeatherService {
   -H 'Accept-Encoding: gzip, deflate, br' 
    
   --compressed`;
+  constructor(private http: HttpClient) {
+
+  }
 
 }
 
